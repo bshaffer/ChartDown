b34b9beb8498f2af50027d9059abaafdf7a58d78
Brent Shaffer <bshafs@gmail.com>
2011-10-02 02:10:45 -0600

    adds 'pre' for text and removes automatic whitespacing

--- data/template/_text.php
+++ data/template/_text.php
@@ -1,9 +1,7 @@
 <tr class="text-row">
     <?php foreach ($row as $bar): ?>
         <?php if ($bar->hasText()): ?>
-            <td colspan="<?php echo $renderer->getColspan($row, $bar, $maxBars) ?>" class="text">
-                <?php echo $bar->renderText() ?>
-            </td>
+            <td colspan="<?php echo $renderer->getColspan($row, $bar, $maxBars) ?>" class="text"><?php echo $bar->renderText() ?></td>
         <?php endif ?>
     <?php endforeach ?>
 </tr>
